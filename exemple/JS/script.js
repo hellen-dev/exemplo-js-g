fd8caecaba28e827e4b7fff05280f495dce7195f
hellen-dev <rm98284@fiap.com.br>
2023-03-07 11:48:58 -0300

    script.js add ao html.

---
+++ exemple/JS/script.js
@@ -0,0 +1,15 @@
+// inline comment
+
+/**
+ * more than
+ * one line
+ * comment
+ */
+
+console.log("Hello, ordinary world")
+
+console.error("There was an error in humanity")
+
+console.warn("Do you agree?")
+
+console.info("they're watching us")
